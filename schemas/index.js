f5007833411f0ff4e6174b1174e865e093abdcfe
bohoon Lee <bohoon100@gmail.com>
2021-09-29 18:01:45 +0900

    sd

--- schemas/index.js
+++ schemas/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const connect = () => {
   mongoose
-    .connect("mongodb://test:test@3.36.71.96:27017/test?authSource=admin", {
+    .connect("mongodb://test:test@3.36.71.96:27017/test", {
       useNewUrlParser: true,
     })
     .catch((err) => console.log(err))
